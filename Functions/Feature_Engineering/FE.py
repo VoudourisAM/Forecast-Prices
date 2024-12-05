51d4f7bc7f0d410e34918c6035de5e7da2ef2c9c
Michel-Angelo <voudouris.am96@gmail.com>
2024-12-05 12:49:49 +0200

    1

--- Functions/Feature_Engineering/FE.py
+++ Functions/Feature_Engineering/FE.py
@@ -4,10 +4,11 @@
 # ---
 # # Import Libraries
 
-# In[3]:
+# In[1]:
 
 
 import pandas as pd
+import pandas_ta as pta
 import numpy as np
 
 
@@ -18,7 +19,7 @@ import numpy as np
 # * Rolling Standar Diviation
 # * Spread
 
-# In[4]:
+# In[2]:
 
 
 class Feature_Engineering:
@@ -43,7 +44,7 @@ class Feature_Engineering:
         This Function of Feature_Engineering return Dataframe with Log Return
         ------------------------------
         Parameter(dataframe): DataFrame
-        Parameter(name_of_column): LIST (One OR More) of dataframe column name 
+        Parameter(name_of_column): LIST of dataframe column name
         ------------------------------
         '''
         
@@ -62,8 +63,8 @@ class Feature_Engineering:
         -Fast Moving Average (FMA): Used for short-term trends, typically with smaller periods like 9, 12, or 20.
         -Slow Moving Average (SMA): Used for long-term trends, typically with larger periods like 50, 100, or 200.
         ------------------------------
-        Parameter(name_of_column): LIST (One OR More) of dataframe column name
-        Parameter(ma_number): Number of Moving Average
+        Parameter(name_of_column): LIST of dataframe column name
+        Parameter(ma_number): Number of Moving Average (Slow - Fast)
         ------------------------------
         '''
         
@@ -79,6 +80,52 @@ class Feature_Engineering:
         except:
             print('No option!\nError')
 #------------------------------------------------------------------------------------------------------------------------------#    
+    def Rolling_Standar_Diviation(self, name_of_column, rsd_number):
+        '''
+        This Function of Feature_Engineering return Dataframe with Rolling Standar Diviation Return
+        -Short-Term Trends Rolling Window Size: 10–20 periods
+        -Long-Term Trends Rolling Window Size: 100–200 periods
+        ------------------------------
+        Parameter(name_of_column): LIST of dataframe column name
+        Parameter(rsd_number): Number of Rolling Standar Diviation
+        ------------------------------
+        '''
+        
+        try:
+            new_dataframe = self.data.copy()
+        
+            for _ in range(len(name_of_column)):
+                new_dataframe[name_of_column[_]+'_RSD_'+str(rsd_number)] = new_dataframe[name_of_column[_]].rolling(rsd_number).std()
+            return new_dataframe
+        except:
+            print('No option!\nError')
+#------------------------------------------------------------------------------------------------------------------------------#    
+    def Relative_Strength_Index(self, name_of_column, rsi_number):
+        '''
+        This Function of Feature_Engineering return Dataframe with Rolling Standar Index Return
+        -Buy: RSI < 10 & z = 1
+        -Sell: RSI > 60 & z = 0
+        
+        -Overbought: RSI > 70 indicates the asset may be overbought and could be due for a pullback.
+        -Oversold: RSI < 30 suggests the asset might be oversold and could be due for a rebound.
+        
+        -RSI > 50 suggests bullish momentum.
+        -RSI < 50 suggests bearish momentum.
+        ------------------------------
+        Parameter(name_of_column): LIST of dataframe column name
+        Parameter(rsi_number): Number of Rolling Standar Index
+        ------------------------------
+        '''
+        
+        try:
+            new_dataframe = self.data.copy()
+        
+            for _ in range(len(name_of_column)):
+                new_dataframe[name_of_column[_]+'_RSI_'+str(rsi_number)] = pta.rsi(new_dataframe[name_of_column[_]], length = rsi_number)
+            return new_dataframe
+        except:
+            print('No option!\nError')
+#-------------------------------------------------------------------------------------------------------------------------------
 
 
 # In[ ]:
